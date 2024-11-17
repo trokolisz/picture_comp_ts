70432084cce2f0a86ed7387cb5cd385492a7e463
trokolisz <dani.csehovics@gmail.com>
2024-11-18 00:27:06 +0100

    Add sidebar and breadcrumb navigation to user management page

--- app/admin/users/page.tsx
+++ app/admin/users/page.tsx
@@ -4,6 +4,20 @@ import Link from 'next/link';
 import { User } from "./columns"
 import { DataTable } from "./data-table"
 
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+
+import { Separator } from "@/components/ui/separator"
+import {
+  SidebarTrigger,
+} from "@/components/ui/sidebar"
+
 async function getUsersFromApi() {
   const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/users`);
   try {
@@ -24,8 +38,46 @@ export default async function Home() {
   const users = await getUsersFromApi();
 
   return (
-    <div className="container mx-auto py-10">
-      <DataTable data={users} />
-    </div>
+    <>
+      <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink href="/">
+                    Home
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className="hidden md:block" />
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink href="/admin/">
+                    Admin
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className="hidden md:block" /> 
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink href="#">
+                    Users
+                  </BreadcrumbLink>
+                </BreadcrumbItem>                   
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+        </header>
+      <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+        <div className="grid auto-rows-min gap-4 md:grid-cols-1">
+          <div className="aspect-video rounded-xl bg-muted/50">
+            <DataTable data={users} />
+          </div>
+
+        </div>
+        <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+      </div>
+
+
+
+    </>
   );
 }
