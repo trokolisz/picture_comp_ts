a54e9531078545e6841b2eb16e32183625427762
biankaszendi <151778989+biankaszendi@users.noreply.github.com>
2024-11-20 14:36:11 +0100

    admin dashboard design

--- FirebaseConfig.tsx
+++ FirebaseConfig.tsx
@@ -1,6 +1,8 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
+import { getFirestore } from 'firebase/firestore';
+
 // ...existing code...
 
 const firebaseConfig = {
@@ -16,7 +18,8 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const database = getDatabase(app);
+const db = getFirestore(app);
 
 // ...existing code...
 
-export { auth, database };
+export { auth, database, db };
